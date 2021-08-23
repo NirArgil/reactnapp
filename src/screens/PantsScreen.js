5ad0615f4fef0a00e2b50b1d000e5155eafd61e5
NIR <MAILMAIL10@GMAIL.COM>
2021-08-23 16:25:52 +0300

    Opt Deploy

--- src/screens/PantsScreen.js
+++ src/screens/PantsScreen.js
@@ -1,17 +1,77 @@
-import React from 'react'
-import { StyleSheet, View, FlatList, Image } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, FlatList, Image, Button, TextInput, ActivityIndicator } from 'react-native';
 import { Text } from 'react-native-elements';
 
 const PantsScreen = () => {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    const getData = async () => {
+        fetch('https://www.mocky.io/v2/5e3940013200005e00ddf87e')
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.log(error))
+            .finally(() => setLoading(false));
+    }
+
+    // const shoesData = data.results.filter(function (result) {
+    //     return result.type === 'shoes';
+    // });
+
+    // console.log(shoesData.length);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
-        <View>
-           <Text h1>PantsScreen</Text> 
+        <View style={styles.container}>
+            <Text h3 style={styles.head}>Pants Screen</Text>
+
+            <Text h3 style={styles.head}>shoesData.length</Text>
+            {isLoading ? <ActivityIndicator /> :
+                <FlatList
+                    data={data.results.sort((a, b) => a.name.localeCompare(b.name))}
+                    keyExtractor={({ _id }, index) => _id}
+                    renderItem={({ item }) => (
+                        <>
+
+                            {item.type === 'pants' &&
+                                <View style={styles.shoeDetails}>
+                                    <Text>
+                                        Name:{item.name} {"\n"}
+                                        Colors: <Button style={item.colors[0]} color={item.colors[0]} title="" />
+                                        
+                                        {item.colors[1] &&
+                                            <Button color={item.colors[1]} onPress={() => navigation.navigate('Home')} title=""></Button>}
+                                        {item.colors[2] &&
+                                            <Button color={item.colors[2]} title=""></Button>}
+                                        {item.colors[3] &&
+                                            <Button color={item.colors[3]} title=""></Button>}
+                                        {item.colors[4] &&
+                                            <Button color={item.colors[4]} title=""></Button>}{"\n"}
+                                        Brand: {item.brand} </Text>
+                                </View>}
+                        </>
+
+                    )}
+                />
+            }
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-  
+    container: {
+        marginBottom: 90,
+    },
+    head: {
+        alignSelf: 'center',
+    },
+    shoeDetails: {
+        marginHorizontal: 50,
+        marginVertical: 45,
+    },
 });
 
 export default PantsScreen;
--- src/screens/ShoesScreen.js
+++ src/screens/ShoesScreen.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { TouchableOpacity } from 'react-native';
-import { StyleSheet, View, FlatList, Image, TextInput, ActivityIndicator } from 'react-native';
-import { Text, Button } from 'react-native-elements';
-import dataApi from '../api/dataApi';
+import { StyleSheet, View, FlatList, Image, Button, TextInput, ActivityIndicator } from 'react-native';
+import { Text } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
-const ShoesScreen = ({ }) => {
+const ShoesScreen = ({ navigation }) => {
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
 
@@ -23,6 +22,26 @@ const ShoesScreen = ({ }) => {
 
     // console.log(shoesData.length);
 
+    const renderShoeSizes = () => {
+        return (
+            selectedItem.sizes.map((item, index) => {
+                return (
+                    <TouchableOpacity key={index} style={{
+                        width: 45,
+                        height: 45,
+                        alignItems: 'center',
+                        borderWidth: 1,
+                        borderRadius: 5
+                    }}>
+                        <Text>
+                            {item}
+                        </Text>
+                    </TouchableOpacity>
+                )
+            })
+        )
+    }
+
     useEffect(() => {
         getData();
     }, []);
@@ -32,17 +51,70 @@ const ShoesScreen = ({ }) => {
             <Text h3 style={styles.head}>Shoes Screen</Text>
 
             <Text h3 style={styles.head}>shoesData.length</Text>
+
             {isLoading ? <ActivityIndicator /> :
                 <FlatList
                     data={data.results.sort((a, b) => a.name.localeCompare(b.name))}
                     keyExtractor={({ _id }, index) => _id}
                     renderItem={({ item }) => (
                         <>
-                            {item.type == 'shoes' &&
+
+                            {item.type === 'shoes' &&
                                 <View style={styles.shoeDetails}>
                                     <Text>
                                         Name:{item.name} {"\n"}
-                                        Colors: <Button title={item.colors[0]}/> {"\n"}
+                                        Colors: {"\n"}
+                                         <TouchableOpacity style={{
+                                            width: '90%',
+                                            height: 30,
+                                            alignItems: 'center',
+                                            borderRadius: 5,
+                                            marginRight: 10,
+                                            backgroundColor: item.colors[0]
+                                        }}>
+                                            <Text>{item.colors[0]}</Text>
+                                        </TouchableOpacity>
+                                 
+
+                                        {item.colors[1] && <TouchableOpacity style={{
+                                            width: '90%',
+                                            height: 30,
+                                            alignItems: 'center',
+                                            backgroundColor: item.colors[1]
+                                        }}>
+                                            <Text>{item.colors[1]}</Text>
+                                        </TouchableOpacity> }
+                                        
+                                        
+
+                                        {item.colors[2] && <TouchableOpacity style={{
+                                            width: '90%',
+                                            height: 30,
+                                            alignItems: 'center',
+                                            backgroundColor: item.colors[2]
+                                        }}>
+                                            <Text>{item.colors[2]}</Text>
+                                        </TouchableOpacity> }
+                        
+                                         
+                                        {item.colors[3] && <TouchableOpacity style={{
+                                            width: '90%',
+                                            height: 30,
+                                            alignItems: 'center',
+                                            backgroundColor: item.colors[3]
+                                        }}>
+                                            <Text>{item.colors[3]}</Text>
+                                        </TouchableOpacity>}
+
+                                        {item.colors[4] && <TouchableOpacity style={{
+                                            width: '90%',
+                                            height: 30,
+                                            alignItems: 'center',
+                                            backgroundColor: item.colors[4]
+                                        }}>
+                                            <Text>{item.colors[4]}</Text>
+                                        </TouchableOpacity>} {"\n"}
+
                                         Brand: {item.brand} </Text>
                                 </View>}
                         </>
@@ -50,20 +122,29 @@ const ShoesScreen = ({ }) => {
                     )}
                 />
             }
+
+            {/* <View>
+                {renderShoeSizes()}
+            </View> */}
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        alignItems: 'center',
         marginBottom: 90,
+        width: "90%",
     },
     head: {
         alignSelf: 'center',
     },
     shoeDetails: {
-        marginHorizontal: 50,
-        marginVertical: 45,
+     
+    },
+    btn: {
+  
     }
 });
 
